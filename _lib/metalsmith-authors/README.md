6301029dadeb142459f92024d0ac969e0cecad2f
dale tan <dale@1stdibs.com>
2015-05-28 19:34:49 -0400

    placeholder modules

---
+++ _lib/metalsmith-authors/README.md
@@ -0,0 +1,8 @@
+# Metalsmith Authors
+
+Create a list of authors contributing to your publication.
+
+### TODO:
+
+- associate all posts with author
+- allow following social network link creations: github, instagram, twitter, facebook (more...)
---
+++ _lib/metalsmith-symlink/lib/index.js
@@ -0,0 +1,19 @@
+/**
+ * User: daletan
+ * Date: 5/27/15
+ * Time: 12:10 PM
+ * Copyright 1stdibs.com, Inc. 2015. All Rights Reserved.
+ */
+
+'use strict';
+
+module.exports = plugin;
+
+function plugin(options) {
+
+    return function (files, metalsmith, done) {
+
+        done();
+    }
+
+}
